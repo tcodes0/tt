fc4b077850d04d83a5a2a2bff81f73e3921de540
Raphael Thomazella <thomazella9@gmail.com>
2018-12-01 21:29:59 -0200

    rename fixture_ttDir to dev_ttDir, add ttFiles constant

--- test/fileSystem_readTtFile.test.ts
+++ test/fileSystem_readTtFile.test.ts
@@ -1,12 +1,12 @@
-import { readTtFile, fixture_ttDir } from "../src/util"
+import { readTtFile, dev_ttDir } from "../src/util"
 import { writeFileSync } from "fs"
 
 describe("readTtFile test", () => {
   test("it works with path and reads history", () => {
     const expected = { history: true }
-    writeFileSync(`${fixture_ttDir}/history.json`, JSON.stringify(expected))
+    writeFileSync(`${dev_ttDir}/history.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: fixture_ttDir,
+      path: dev_ttDir,
       file: "history.json",
     })
     expect(result).toEqual(expected)
@@ -14,9 +14,9 @@ describe("readTtFile test", () => {
 
   test("it works with path and reads state", () => {
     const expected = { state: true }
-    writeFileSync(`${fixture_ttDir}/state.json`, JSON.stringify(expected))
+    writeFileSync(`${dev_ttDir}/state.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: fixture_ttDir,
+      path: dev_ttDir,
       file: "state.json",
     })
     expect(result).toEqual(expected)
@@ -24,9 +24,9 @@ describe("readTtFile test", () => {
 
   test("it works with path and defaults to state", () => {
     const expected = { state: true }
-    writeFileSync(`${fixture_ttDir}/state.json`, JSON.stringify(expected))
+    writeFileSync(`${dev_ttDir}/state.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: fixture_ttDir,
+      path: dev_ttDir,
     })
     expect(result).toEqual(expected)
   })
--- test/fileSystem_writeTtFile.test.ts
+++ test/fileSystem_writeTtFile.test.ts
@@ -1,49 +1,46 @@
-import { writeTtFile, fixture_ttDir } from "../src/util"
+import { writeTtFile, dev_ttDir } from "../src/util"
 import { readFileSync } from "fs"
 import rm from "rimraf"
 
 describe("writeTtFile test", () => {
   test("reads history", () => {
     const expected = { history: true }
-    writeTtFile({ path: fixture_ttDir, file: "history.json", data: expected })
-    const result = readFileSync(`${fixture_ttDir}/history.json`, "utf-8")
+    writeTtFile({ path: dev_ttDir, file: "history.json", data: expected })
+    const result = readFileSync(`${dev_ttDir}/history.json`, "utf-8")
     const parsed = JSON.parse(result)
     expect(parsed).toEqual(expected)
   })
 
   test("reads state", () => {
     const expected = { state: true }
-    writeTtFile({ path: fixture_ttDir, file: "state.json", data: expected })
-    const result = readFileSync(`${fixture_ttDir}/state.json`, "utf-8")
+    writeTtFile({ path: dev_ttDir, file: "state.json", data: expected })
+    const result = readFileSync(`${dev_ttDir}/state.json`, "utf-8")
     const parsed = JSON.parse(result)
     expect(parsed).toEqual(expected)
   })
 
   test("defauts file to state", () => {
     const expected = { state: true }
-    writeTtFile({ path: fixture_ttDir, data: expected })
-    const result = readFileSync(`${fixture_ttDir}/state.json`, "utf-8")
+    writeTtFile({ path: dev_ttDir, data: expected })
+    const result = readFileSync(`${dev_ttDir}/state.json`, "utf-8")
     const parsed = JSON.parse(result)
     expect(parsed).toEqual(expected)
   })
 
   test("defauts data to {}", () => {
     const expected = {}
-    writeTtFile({ path: fixture_ttDir })
-    const result = readFileSync(`${fixture_ttDir}/state.json`, "utf-8")
+    writeTtFile({ path: dev_ttDir })
+    const result = readFileSync(`${dev_ttDir}/state.json`, "utf-8")
     const parsed = JSON.parse(result)
     expect(parsed).toEqual(expected)
   })
 
   test("it attempts mkdir when can't write", () => {
     const expected = {}
-    writeTtFile({ path: `${fixture_ttDir}/somefolder` })
-    const result = readFileSync(
-      `${fixture_ttDir}/somefolder/state.json`,
-      "utf-8",
-    )
+    writeTtFile({ path: `${dev_ttDir}/somefolder` })
+    const result = readFileSync(`${dev_ttDir}/somefolder/state.json`, "utf-8")
     const parsed = JSON.parse(result)
+    rm(`${dev_ttDir}/somefolder`, () => {})
     expect(parsed).toEqual(expected)
-    rm(`${fixture_ttDir}/somefolder`, () => {})
   })
 })
