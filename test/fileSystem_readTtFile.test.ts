d5ebd1f0a93ce65e7eecf113a83186b26aa4361a
Raphael Thomazella <thomazella9@gmail.com>
2018-12-09 23:39:35 -0200

    try fix ci

--- test/fileSystem_readTtFile.test.ts
+++ test/fileSystem_readTtFile.test.ts
@@ -1,12 +1,19 @@
 import { readTtFile, dev_ttDir } from "../src/util"
 import { writeFileSync } from "fs"
+import { execSync } from "child_process"
+
+const testDir = `${dev_ttDir}-readTtFile.test`
+
+beforeAll(() => {
+  execSync(`mkdir -p ${testDir}`)
+})
 
 describe("readTtFile test", () => {
   test("it works with path and reads history", () => {
     const expected = { history: true }
-    writeFileSync(`${dev_ttDir}/history.json`, JSON.stringify(expected))
+    writeFileSync(`${testDir}/history.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: dev_ttDir,
+      path: testDir,
       file: "history.json",
     })
     expect(result).toEqual(expected)
@@ -14,9 +21,9 @@ describe("readTtFile test", () => {
 
   test("it works with path and reads state", () => {
     const expected = { state: true }
-    writeFileSync(`${dev_ttDir}/state.json`, JSON.stringify(expected))
+    writeFileSync(`${testDir}/state.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: dev_ttDir,
+      path: testDir,
       file: "state.json",
     })
     expect(result).toEqual(expected)
@@ -24,9 +31,9 @@ describe("readTtFile test", () => {
 
   test("it works with path and defaults to state", () => {
     const expected = { state: true }
-    writeFileSync(`${dev_ttDir}/state.json`, JSON.stringify(expected))
+    writeFileSync(`${testDir}/state.json`, JSON.stringify(expected))
     const result = readTtFile({
-      path: dev_ttDir,
+      path: testDir,
     })
     expect(result).toEqual(expected)
   })
