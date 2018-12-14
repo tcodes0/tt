3d321d3479dd6356d448885bfa92f8e59f175112
Raphael Thomazella <thomazella9@gmail.com>
2018-12-14 12:02:48 -0200

    realize appendFile is too trivial and rm it

--- src/core/saga_stop.ts
+++ src/core/saga_stop.ts
@@ -1,11 +1,11 @@
 import { takeEvery, select, put } from "redux-saga/effects"
 import { TASK_STOP } from "./action_taskStop"
 import taskUnset from "./action_taskUnset"
-import { State, historyAdd, HISTORY_ADD } from "../core"
+import { State, historyAdd } from "../core"
 import { Action } from "../util"
-import { PayloadHistoryAdd } from "./action_historyAdd"
+import { PayloadHistoryAdd, HISTORY_ADD } from "./action_historyAdd"
 
-function* sagaStop() {
+function* stopTask() {
   const task: State["task"] = yield select<State>(state => state.task)
   task.sprints[0].end = Date.now()
 
@@ -20,7 +20,7 @@ function* sagaHistory(action: Action<PayloadHistoryAdd>) {
   yield 33
 }
 
-export default function* stateWriteSaga() {
-  yield takeEvery(TASK_STOP, sagaStop)
+export default function* sagaStop() {
+  yield takeEvery(TASK_STOP, stopTask)
   yield takeEvery(HISTORY_ADD, () => {})
 }
